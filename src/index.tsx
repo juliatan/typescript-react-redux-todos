e2a56b57c1ee11185c97245d35c31e523a3e8034
Julia Tan <juliajtan@gmail.com>
2020-10-31 16:14:12 +0000

    Add state and counter buttons

--- src/index.tsx
+++ src/index.tsx
@@ -2,13 +2,29 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 interface AppProps {
-  color: string;
+  color?: string;
 }
 
 class App extends Component<AppProps> {
+  state = { counter: 0 };
+
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
+
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
+
   render() {
-    return <div>{this.props.color}</div>;
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
   }
 }
 
-ReactDOM.render(<App color="red" />, document.querySelector('#root'));
+ReactDOM.render(<App />, document.querySelector('#root'));
