dad9fcd4402081aba81a9fb91ac9e0e03a9d7e1f
Julia Tan <juliajtan@gmail.com>
2020-10-31 16:17:00 +0000

    An example of how we'd define a functional component in Typescript

--- src/index.tsx
+++ src/index.tsx
@@ -5,26 +5,30 @@ interface AppProps {
   color?: string;
 }
 
-class App extends Component<AppProps> {
-  state = { counter: 0 };
+const App = (props: AppProps): JSX.Element => {
+  return <div>{props.color}</div>;
+};
 
-  onIncrement = (): void => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
+// class App extends Component<AppProps> {
+//   state = { counter: 0 };
 
-  onDecrement = (): void => {
-    this.setState({ counter: this.state.counter - 1 });
-  };
+//   onIncrement = (): void => {
+//     this.setState({ counter: this.state.counter + 1 });
+//   };
 
-  render() {
-    return (
-      <div>
-        <button onClick={this.onIncrement}>Increment</button>
-        <button onClick={this.onDecrement}>Decrement</button>
-        {this.state.counter}
-      </div>
-    );
-  }
-}
+//   onDecrement = (): void => {
+//     this.setState({ counter: this.state.counter - 1 });
+//   };
 
-ReactDOM.render(<App />, document.querySelector('#root'));
+//   render() {
+//     return (
+//       <div>
+//         <button onClick={this.onIncrement}>Increment</button>
+//         <button onClick={this.onDecrement}>Decrement</button>
+//         {this.state.counter}
+//       </div>
+//     );
+//   }
+// }
+
+ReactDOM.render(<App color="red" />, document.querySelector('#root'));
