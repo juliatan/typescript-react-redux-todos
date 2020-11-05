e50539bc2d6f8d7bffc8d51230d88073eb73ed56
Julia Tan <julia@joinzoe.com>
2020-11-05 08:59:02 +0000

    Switch back to class based component

--- src/index.tsx
+++ src/index.tsx
@@ -5,30 +5,30 @@ interface AppProps {
   color?: string;
 }
 
-const App = (props: AppProps): JSX.Element => {
-  return <div>{props.color}</div>;
-};
+// const App = (props: AppProps): JSX.Element => {
+//   return <div>{props.color}</div>;
+// };
 
-// class App extends Component<AppProps> {
-//   state = { counter: 0 };
+class App extends Component<AppProps> {
+  state = { counter: 0 };
 
-//   onIncrement = (): void => {
-//     this.setState({ counter: this.state.counter + 1 });
-//   };
+  onIncrement = (): void => {
+    this.setState({ counter: this.state.counter + 1 });
+  };
 
-//   onDecrement = (): void => {
-//     this.setState({ counter: this.state.counter - 1 });
-//   };
+  onDecrement = (): void => {
+    this.setState({ counter: this.state.counter - 1 });
+  };
 
-//   render() {
-//     return (
-//       <div>
-//         <button onClick={this.onIncrement}>Increment</button>
-//         <button onClick={this.onDecrement}>Decrement</button>
-//         {this.state.counter}
-//       </div>
-//     );
-//   }
-// }
+  render() {
+    return (
+      <div>
+        <button onClick={this.onIncrement}>Increment</button>
+        <button onClick={this.onDecrement}>Decrement</button>
+        {this.state.counter}
+      </div>
+    );
+  }
+}
 
 ReactDOM.render(<App color="red" />, document.querySelector('#root'));
