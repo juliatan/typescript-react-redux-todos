8f148cdd2e6e9d8845ee6ff9c4fb52f24578abac
Julia Tan <julia@joinzoe.com>
2020-11-06 16:07:09 +0000

    Refactor actions by defining a new Type of Action, so it can easily be imported into reducers

--- src/actions/types.ts
+++ src/actions/types.ts
@@ -1,6 +1,10 @@
+import { FetchTodosAction, DeleteTodoAction } from './todos';
+
 export enum ActionTypes {
   // we don't actually care about making this a string e.g. fetchTodos = 'FETCH_TODOS'
   // leaving is like this, Typescript automatically assigns fetchTodos = 0 since it's an enum
   fetchTodos,
   deleteTodo,
 }
+
+export type Action = FetchTodosAction | DeleteTodoAction;
--- src/reducers/todos.ts
+++ src/reducers/todos.ts
@@ -1,7 +1,7 @@
-import { Todo, FetchTodosAction, ActionTypes } from '../actions/index';
+import { Todo, Action, ActionTypes } from '../actions/index';
 
 // notation is saying state takes the shape of an array of Todo, and the initial value of state = []
-export const todosReducer = (state: Todo[] = [], action: FetchTodosAction) => {
+export const todosReducer = (state: Todo[] = [], action: Action) => {
   switch (action.type) {
     case ActionTypes.fetchTodos:
       return action.payload;
