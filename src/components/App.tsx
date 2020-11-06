0e9edae84ca37a5f6c04330c23703e6f3affea37
Julia Tan <julia@joinzoe.com>
2020-11-06 15:27:01 +0000

    Hook up action and reducer state to App

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,7 +1,24 @@
 import React from 'react';
+import { connect } from 'react-redux';
 
-export class App extends React.Component {
+import { Todo, fetchTodos } from '../actions';
+import { StoreState } from '../reducers';
+
+// Define what type of props we want App to take
+interface AppProps {
+  todos: Todo[];
+  fetchTodos(): any;
+}
+class _App extends React.Component<AppProps> {
   render() {
     return <div>Hi there</div>;
   }
 }
+
+// state argument should take the shape of StoreState
+// mapStateToProps function should return an object with todos key and array of Todo
+const mapStateToProps = (state: StoreState): { todos: Todo[] } => {
+  return { todos: state.todos };
+};
+
+export const App = connect(mapStateToProps, { fetchTodos })(_App);
