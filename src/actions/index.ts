15b4ecf3c3ec4782a426c22a2be8f67d645b0f5e
Julia Tan <julia@joinzoe.com>
2020-11-06 14:56:48 +0000

    Defined an action and its type

---
+++ src/actions/index.ts
@@ -0,0 +1,30 @@
+import axios from 'axios';
+import { Dispatch } from 'redux'; // from redux docs, we see they have a Dispatch interface we can use
+import { ActionTypes } from './types';
+
+// define what we expect to get back from API...
+interface Todo {
+  id: number;
+  title: string;
+  completed: boolean;
+}
+
+// option, but good practice to define exactly the type of what should be disptactched
+interface FetchTodosAction {
+  type: ActionTypes.fetchTodos;
+  payload: Todo[];
+}
+
+const url = 'https://jsonplaceholder.typicode.com/todos';
+
+export const fetchTodos = () => {
+  return async (dispatch: Dispatch) => {
+    // ...then tell axios.get we expect to get back an array of Todos
+    const response = await axios.get<Todo[]>(url);
+    dispatch<FetchTodosAction>({
+      // type: 'FETCH_TODOS',
+      type: ActionTypes.fetchTodos,
+      payload: response.data,
+    });
+  };
+};
